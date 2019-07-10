3294bda2840c8453664debbba11680fc60e8ca7c
Bryan <ybcheong@starhub.com>
2019-07-10 10:45:12 +0800

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -2,6 +2,7 @@ import netmiko
 from netmiko import ConnectHandler
 
 
+def add_changes_router(prefixname, action, ipprefix)
 ssh_connection = ConnectHandler(
    device_type ='cisco_xr',
    ip='172.16.14.201',
@@ -9,9 +10,12 @@ ssh_connection = ConnectHandler(
    password='sgnog'
     )
 
-result = ssh_connection.find_prompt() + "\n"
+#ssh_connection.find_prompt() + "\n"
 
-result += ssh_connection.send_command("show version", delay_factor=2)
+#ssh_connection.send_command("config t", delay_factor=2)
+
+result = net_connect.send_config_set(['interface g0/0/0/3', 'no shut'])
+
 
 print(result)
 
