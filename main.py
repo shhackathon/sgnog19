a6c59a7af3eb044c1a2f79989ce10327b1061026
NGUYEN Duy Cuong <duycuong.nguyen@starhub.com>
2019-07-10 14:31:58 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from push_change import push_change
+from get_details import get_details
+
+
+def update_prefix(prefixset_name, prefix, operation):
+    result = push_change(
+        prefixset_name,
+        prefix,
+        operation,
+    )
+
+    if result is True:
+        print("GOOD")
+    else:
+        print("BAD")
+
+
+
+def main():
+    sid = "Service01"
+    prefix = "10.0.0.0/24"
+    operation = "add"
+
+    info = get_details(sid)
+    prefixset_name, current_prefixes = info[-2:]
+    print(f"{sid} / {prefixset_name} / {current_prefixes}")
+
+    if prefixset_name:
+        # update_prefix(prefixset_name, prefix, operation)
+        pass
+
+if __name__ == "__main__":
+    main()
