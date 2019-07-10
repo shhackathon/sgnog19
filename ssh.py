9f201a916b035c7d7e78a20716ca1db064bb4c1b
Bryan <ybcheong@starhub.com>
2019-07-10 10:35:24 +0800

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -2,17 +2,18 @@ import netmiko
 from netmiko import ConnectHandler
 
 
-   ssh_connection = ConnectHandler(
-      device_type ='cisco_xr',
-      ip=172.16.14.201,
-      username=sgnog,
-      password=sgnog
+ssh_connection = ConnectHandler(
+   device_type ='cisco_xr',
+   ip=172.16.14.201,
+   username=sgnog,
+   password=sgnog
     )
 
-   result = ssh_connection.find_prompt() + "\n"
+result = ssh_connection.find_prompt() + "\n"
 
-   result += ssh_connection.send_command("show version", delay_factor=2)
+result += ssh_connection.send_command("show version", delay_factor=2)
 
-   ssh_connection.disconnect()
-   print (result)
+print(result)
+
+ssh_connection.disconnect()
 
