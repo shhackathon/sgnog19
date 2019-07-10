5bc6882e7bd3023176f26fc4add6f33eba148888
John <jacraluta@gmail.com>
2019-07-10 15:22:20 +0800

    Update get_details.py

--- get_details.py
+++ get_details.py
@@ -38,7 +38,8 @@ def get_details(SID):
 #                prefixset_new.append(j.split(',\n')[0])
 #            elif '\n' in j:
 #                prefixset_new.append(j.split('\n')[0])
-    return iface,iface_ip,nbr_ip,rpl,prefixset_name,(' ').join(prx_set_cfg.ioscfg)
+    diff_libinput = (' ').join(prx_set_cfg.ioscfg) + ' end set\n'
+    return iface,iface_ip,nbr_ip,rpl,prefixset_name,diff_libinput
 
 
 def find_nxthop(int_ip,mask):
